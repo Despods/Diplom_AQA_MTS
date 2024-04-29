6d5ad1dbc848eb040d69e6611134386d42f8b9f2
Zhdak <vitas579@mail.ru>
2024-04-29 17:38:26 +0500

    allure cosmetic

--- Diplom_AQA_MTS/Tests/LoginTest.cs
+++ Diplom_AQA_MTS/Tests/LoginTest.cs
@@ -1,23 +1,31 @@
 ﻿using Diplom_AQA_MTS.Pages;
 using Diplom_AQA_MTS.Steps;
+using Allure.Net.Commons;
+using NUnit.Allure.Attributes;
 
 namespace Diplom_AQA_MTS.Tests
 {
+    [AllureSuite("UI Login Tests")]
     public class LoginTest : BaseTest
     {
         [Test]
+        [AllureName("Логин с передачей корректных параметров")]
         public void CorrectLoginTest()
         {
             Assert.That(NavigationSteps.SuccessfulLogin(Admin).WelcomeMessage.Displayed);
+            AllureApi.Step("Вход выполнен успешно");
         }
 
         [Test]
+        [AllureName("Логин с передачей некорректных параметров")]
+        [AllureDescription("Тест использование некорректных данных")]
         public void UnCorrectLoginTest()
         {
             string ErrorText = "Either your email address or your password is wrong. Please try again or recover your password.";
             var LoginPage = new LoginPage(Driver);
             LoginPage.IncorrectLogin("Incorect@mail.ru", "Login");
             Assert.That(LoginPage.ErrorLabel.Text.Trim(), Is.EqualTo(ErrorText));
+            AllureApi.Step($"Получена корректная ошибка : ", {ErrorText});
         }
     }
 }
--- Diplom_AQA_MTS/Tests/ProfileTest.cs
+++ Diplom_AQA_MTS/Tests/ProfileTest.cs
@@ -1,11 +1,15 @@
 ﻿using Diplom_AQA_MTS.Pages;
 using Diplom_AQA_MTS.Steps;
+using Allure.Net.Commons;
+using NUnit.Allure.Attributes;
 
 namespace Diplom_AQA_MTS.Tests
 {
+    [AllureSuite("UI ProfileSettings Tests")]
     public class ProfileTest : BaseTest
     {
         [Test]
+        [AllureName("Изменение аватара профиля")]
         public void UploadAvatarTest()
         {
             NavigationSteps.SuccessfulLogin(Admin);
@@ -14,6 +18,7 @@ namespace Diplom_AQA_MTS.Tests
             var ProfileSettingsPage = new ProfileSettingsPage(Driver,true);
             ProfileSettingsPage.ProfileUploadAvatar();
             Assert.That(ProfileSettingsPage.AvatarImage.Displayed);
+            AllureApi.Step("Аватар успешно добавлен");
             ProfileSettingsPage.ProfileDeleteAvatar();
         }
     }
